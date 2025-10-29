159e81a85907cfb5ba850eaedc641588fa3adf86
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-10-29 09:00:59 -0300

    refactor: changing function call

--- src/main/java/org/upcy/simbia/dataprovider/persistence/repository/LoginRepository.java
+++ src/main/java/org/upcy/simbia/dataprovider/persistence/repository/LoginRepository.java
@@ -6,11 +6,7 @@ import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.upcy.simbia.dataprovider.persistence.entity.Login;
 
-import java.util.Optional;
-
 public interface LoginRepository extends JpaRepository<Login, Long> {
-
-    Optional<Login> findByUserName(String userName);
 
     @Query(value = "SELECT FN_TableIdGenerator('loginindustry', 'idloginindustry')", nativeQuery = true)
     Long generateId();
@@ -18,7 +14,7 @@ public interface LoginRepository extends JpaRepository<Login, Long> {
     @Procedure(procedureName = "sp_updateuserpassword")
     void updateUserPassword(@Param("acusername") String userName, @Param("acpassword") String newPassword);
 
-    @Query(value = "SELECT fn_validatelogin(:username, :password)", nativeQuery = true)
+    @Query(value = "SELECT fn_loginindustry(:username, :password)", nativeQuery = true)
     Boolean validateUser(@Param("username") String username, @Param("password") String password);
 
 }
